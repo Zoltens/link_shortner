1c324fc74e908c0e1e2ac7c03d114b320e9eef10
Andrei Zaitsau <zolten_51@mail.ru>
2022-07-09 21:39:51 +0300

    deploy3

--- short_link/urls.py
+++ short_link/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import show_hello, Log, Out, Registr, short_link, redirect_to_orig, profile
 
 urlpatterns = [
-    path('main/', show_hello, name='main'),
+    path('', show_hello, name='main'),
     path('login/', Log.as_view(), name='login'),
     path('logout/', Out.as_view(), name='logout'),
     path('registration/', Registr.as_view(), name='registration'),
