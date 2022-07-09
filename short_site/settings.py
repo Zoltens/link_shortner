cc7ce6bd64048c4964a254324f327d3c4111d052
Andrei Zaitsau <zolten_51@mail.ru>
2022-07-09 21:36:38 +0300

    deploy3

--- short_site/settings.py
+++ short_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(o&#-dzwr=-r=ekn+g)hqbqp)@&%643wdk#x*fzm(3$9g89ov*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
