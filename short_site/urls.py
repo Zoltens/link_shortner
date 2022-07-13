dd2e63420c0e2d382388ddd5cf0794da6f695de5
Andrei Zaitsau <zolten_51@mail.ru>
2022-07-13 15:14:58 +0300

    add whitenoise

--- short_site/urls.py
+++ short_site/urls.py
@@ -18,9 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('short_link.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
