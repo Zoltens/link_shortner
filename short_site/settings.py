c603c1291e18de4a57741335df01ef3ecc3a075d
Andrei Zaitsau <a.zajcew@gmail.com>
2022-07-14 16:23:52 +0300

    clean old conf

--- short_site/settings.py
+++ short_site/settings.py
@@ -74,16 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'short_site.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -129,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
